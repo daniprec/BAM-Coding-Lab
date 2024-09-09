4b4469e73372cb5fb4af5add69c2a4b0c1537daa
Daniel Precioso <danipg1409@gmail.com>
2024-09-09 20:11:00 +0200

    Add solutions to exercise 11

--- solutions/11_fundamentals.py
+++ solutions/11_fundamentals.py
@@ -6,6 +6,8 @@
 
 def power(a: int, b: int) -> int:
     result = a**b
+    # The return statement was missing
+    return result
 
 
 print(power(2, 3))  # 8
@@ -14,8 +16,11 @@ print(power(2, 3))  # 8
 
 
 def factorial(n: int) -> int:
-    result = 0
-    for i in range(1, n):
+    # We must initialize the result to 1
+    # because we are multiplying!
+    result = 1
+    # The range should go up to n+1
+    for i in range(1, n + 1):
         result *= i
     return result
 
@@ -25,7 +30,8 @@ print(factorial(4))  # 24
 # ----------------------------------------------
 
 
-def power_or_factorial(a: int, b: int = 0) -> int:
+def power_or_factorial(a: int, b: int = None) -> int:
+    # b is not None, it is 0 by default
     if b is None:
         return factorial(a)
     else:
@@ -42,11 +48,15 @@ def count_even_and_odd(ls: list[int]) -> tuple:
     n_even = 0
     n_odd = 0
     for num in ls:
-        if num // 2 == 0:
+        # The condition should be num % 2 == 0
+        # Remember that // is integer division
+        if num % 2 == 0:
             n_even += 1
         else:
-            n_odd -= 1
-    return n_even + n_odd
+            # Should be summing up the odd numbers
+            n_odd += 1
+    # We want to return both counts, not the sum!
+    return n_even, n_odd
 
 
 print(count_even_and_odd([1, 2, 3, 4, 5]))  # (2, 3)
