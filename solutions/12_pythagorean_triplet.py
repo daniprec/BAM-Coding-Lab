5ca7519808d35c4a0a40688ac5a0ea8f17f4ed32
Daniel Precioso <danipg1409@gmail.com>
2024-09-09 20:17:47 +0200

    Solution of exercise 12

--- solutions/12_pythagorean_triplet.py
+++ solutions/12_pythagorean_triplet.py
@@ -1,3 +1,5 @@
+import time
+
 # Step 1: We create a function to check whether
 # a list of points is a Pythagorean triplet
 
@@ -35,6 +37,8 @@ print(is_pythagorean([4, 5, 3]))  # True
 # Step 2: Create a loop to check all possible triplets
 # We first test its speed with a lower limit
 
+print("\nIMPLEMENTATION 1\n")
+
 limit = 100
 
 # Initialize the list that will contain all triples
@@ -42,7 +46,39 @@ ls_triplets = []
 # Initialize the count of iterations
 niter = 0
 
+# Bad implementation
+time_start = time.time()
+for c in range(1, limit):
+    for b in range(1, limit):
+        for a in range(1, limit):
+            # Iteration count goes up
+            niter += 1
+            if (a + b + c) > limit:
+                continue
+            triplet = sorted([a, b, c])
+            if is_pythagorean(triplet) and (triplet not in ls_triplets):
+                ls_triplets.append(triplet)
+time_end = time.time()
+
+print(f"There are {len(ls_triplets)} triplets with a sum less than {limit}")
+print(f"We performed {niter} iterations")  # Should be 99^3 iterations
+print(f"Time taken: {time_end - time_start}")
+
+# ----------------------------------------------
+
+# Step 3: Improve the loop, make sure we count 17 triplets
+
+print("\nIMPLEMENTATION 2\n")
+
+limit = 500
+
+# Initialize the list that will contain all triples
+ls_triplets = []
+# Initialize the count of iterations
+niter = 0
+
 # Better implementation: Know that c > b > a
+time_start = time.time()
 for c in range(1, limit):
     for b in range(1, c):
         for a in range(1, b):
@@ -53,7 +89,42 @@ for c in range(1, limit):
             triplet = sorted([a, b, c])
             if is_pythagorean(triplet):
                 ls_triplets.append(triplet)
+time_end = time.time()
 
 
-print(f"There are {len(ls_triplets)}")
+print(f"There are {len(ls_triplets)} triplets with a sum less than {limit}")
 print(f"We performed {niter} iterations")
+print(f"Time taken: {time_end - time_start}")
+
+# ----------------------------------------------
+
+# Step 4: Improve the loop even more
+
+print("\nIMPLEMENTATION 3\n")
+
+limit = 5000
+
+# Initialize the list that will contain all triples
+ls_triplets = []
+# Initialize the count of iterations
+niter = 0
+
+# Better implementation: Know that c > b > a
+time_start = time.time()
+for c in range(1, limit):
+    for b in range(1, min(limit - c, c)):
+        # Iteration count goes up
+        niter += 1
+        a = (c**2 - b**2) ** 0.5
+        if int(a) != a:
+            continue
+        if a > b:
+            continue
+        if (a + b + c) > limit:
+            continue
+        ls_triplets.append(sorted([a, b, c]))
+time_end = time.time()
+
+print(f"There are {len(ls_triplets)} triplets with a sum less than {limit}")
+print(f"We performed {niter} iterations")
+print(f"Time taken: {time_end - time_start}")
