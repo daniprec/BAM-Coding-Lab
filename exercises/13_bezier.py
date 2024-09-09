5cd78af30cd651ce2f21d6d37036857add2c2731
Daniel Precioso <danipg1409@gmail.com>
2024-09-09 20:24:01 +0200

    Bezier exercise and solution

--- exercises/13_bezier.py
+++ exercises/13_bezier.py
@@ -10,34 +10,20 @@ def bezier_cubic(
     """
     Generate a cubic Bézier curve with four control points
     """
-    t = np.linspace(0, 1, n_points).reshape(-1, 1)
-    curve = (
-        (np.power(1 - t, 3) * p0)
-        + (3 * np.power(1 - t, 2) * t * p1)
-        + (3 * (1 - t) * np.power(t, 2) * p2)
-        + (np.power(t, 3) * p3)
-    )
-    x = curve[:, 0]
-    y = curve[:, 1]
-    return x, y
+    # Initialize the parameter t
+    pass
+    # Compute the curve
+    pass
+    # Extract the x and y coordinates
+    pass
 
 
 # Lets try it out with some control points
-p0 = (0, 0)
-p1 = (0.3, 0.5)
-p2 = (0.7, 0.2)
-p3 = (1, 1)
+
 # Generate the curve and plot it
-x, y = bezier_cubic(p0, p1, p2, p3)
-plt.plot(x, y)
+
 # Plot the control points on it
-x_pt = np.array([p[0] for p in [p0, p1, p2, p3]])
-y_pt = np.array([p[1] for p in [p0, p1, p2, p3]])
-plt.plot(x_pt, y_pt, "r.--")
-plt.axis("equal")
-plt.title("Cubic Bezier Curve")
-plt.show()
-plt.close()
+
 
 # ----------------------------------------------
 
@@ -47,72 +33,32 @@ plt.close()
 
 def area_under_curve(x: np.array, y: np.array) -> float:
     """Approximage the area under a curve using the trapezoidal rule"""
-    dx = np.abs(np.diff(x))
-    area = (dx * y[:-1]).sum()
-    return area
+    pass
 
 
 def length_of_curve(x: np.array, y: np.array) -> float:
     """Approximage the length of a curve using the distance formula"""
-    lc = np.sum(np.sqrt((np.diff(x) ** 2) + (np.diff(y) ** 2)))
-    return lc
+    pass
 
 
 # Lets try it out with (y = 1 - x)
-x = np.linspace(0, 1, 1000)
-y = 1 - x
-plt.plot(x, y, "b")
-plt.plot(x, 0 * x, "k--")
-plt.plot(0 * x, y, "k--")
-plt.axis("equal")
-plt.title("y = 1 - x")
-plt.show()
-plt.close()
 
 # The area w.r.t. the axes is 0.5
-area = area_under_curve(x, y)
-print(f"Area: {area}")
 
 # Its length is sqrt(2) ~= 1.414
-lc = length_of_curve(x, y)
-print(f"Length: {lc}")
 
 # ----------------------------------------------
 
 # Step 3: Test functions with some sample control points
-v = 0.35
-p0 = (1, 0)
-p1 = (1, v)
-p2 = (v, 1)
-p3 = (0, 1)
-x, y = bezier_cubic(p0, p1, p2, p3)
 
-area = area_under_curve(x, y)
-error = (area - (np.pi / 4)) / (np.pi / 4) * 100
-print(f"For v = {v:.2f} the percentage error in area is {error:.2f}%")
+# Define a v, generate a Bézier curve with it
 
-l_approx = length_of_curve(x, y)
-error = (l_approx - (np.pi / 2)) / (np.pi / 2) * 100
-print(f"For v = {v:.2f} the percentage error in length is {error:.2f}%")
+# Compute the area and length of the curve
 
-plt.figure(figsize=(4, 4))
-x_pt = np.array([p[0] for p in [p0, p1, p2, p3]])
-y_pt = np.array([p[1] for p in [p0, p1, p2, p3]])
-plt.plot(x_pt, y_pt, "r.--")
-plt.plot(x, y, "b")
-plt.plot(x, 0 * x, "k--")
-plt.plot(0 * x, y, "k--")
+# Plot the curve and the control points
 
 # Draw an actual quarter circle
-t = np.linspace(0, np.pi / 2, 100)
-x_circle = np.cos(t)
-y_circle = np.sin(t)
-plt.plot(x_circle, y_circle, "k-")
 
-plt.axis("equal")
-plt.title(f"Cubic Bezier Curve with v = {v}")
-plt.show()
-plt.close()
 
 # ----------------------------------------------
 # BONUS: Implement a function to generate a Bezier curve
--- solutions/13_bezier.py
+++ solutions/13_bezier.py
@@ -8,13 +8,16 @@ def bezier_cubic(p0: tuple, p1: tuple, p2: tuple, p3: tuple, n_points: int = 100
     """
     Generate a cubic Bézier curve with four control points
     """
+    # Initialize the parameter t
     t = np.linspace(0, 1, n_points).reshape(-1, 1)
+    # Compute the curve
     curve = (
         (np.power(1 - t, 3) * p0)
         + (3 * np.power(1 - t, 2) * t * p1)
         + (3 * (1 - t) * np.power(t, 2) * p2)
         + (np.power(t, 3) * p3)
     )
+    # Extract the x and y coordinates
     x = curve[:, 0]
     y = curve[:, 1]
     return x, y
@@ -77,6 +80,8 @@ print(f"Length: {lc}")
 # ----------------------------------------------
 
 # Step 3: Test functions with some sample control points
+
+# Define a v, generate a Bézier curve with it
 v = 0.35
 p0 = (1, 0)
 p1 = (1, v)
@@ -84,6 +89,7 @@ p2 = (v, 1)
 p3 = (0, 1)
 x, y = bezier_cubic(p0, p1, p2, p3)
 
+# Compute the area and length of the curve
 area = area_under_curve(x, y)
 error = (area - (np.pi / 4)) / (np.pi / 4) * 100
 print(f"For v = {v:.2f} the percentage error in area is {error:.2f}%")
@@ -92,6 +98,7 @@ l_approx = length_of_curve(x, y)
 error = (l_approx - (np.pi / 2)) / (np.pi / 2) * 100
 print(f"For v = {v:.2f} the percentage error in length is {error:.2f}%")
 
+# Plot the curve and the control points
 plt.figure(figsize=(4, 4))
 x_pt = np.array([p[0] for p in [p0, p1, p2, p3]])
 y_pt = np.array([p[1] for p in [p0, p1, p2, p3]])
