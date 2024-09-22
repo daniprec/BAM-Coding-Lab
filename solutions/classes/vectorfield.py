a9150adcb42367392101a9a236ef280bf333a682
Daniel Precioso <danipg1409@gmail.com>
2024-09-22 21:18:04 +0200

    Move both vectorfields to the same class

--- solutions/classes/vectorfield.py
+++ solutions/classes/vectorfield.py
@@ -4,10 +4,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def horizontal_current(x: float, y: float, strength: float = 1) -> Tuple[float, float]:
+    """This function returns the velocity field of a horizontal current at a given point.
+
+    Parameters
+    ----------
+    x : float
+        Position in the x-direction
+    y : float
+        Position in the y-direction
+
+    Returns
+    -------
+    Tuple[float, float]
+        The velocity field at the given point
+    """
+    u = -x * strength
+    v = 0
+    return u, v
+
+
+def circular_current(x: float, y: float, strength: float = 1) -> Tuple[float, float]:
+    """This function returns the velocity field of a circular current at a given point.
+
+    Parameters
+    ----------
+    x : float
+        Position in the x-direction
+    y : float
+        Position in the y-direction
+
+    Returns
+    -------
+    Tuple[float, float]
+        The velocity field at the given point
+    """
+    u = -y * strength
+    v = x * strength
+    return u, v
+
+
 class VectorField:
 
-    def __init__(self, strength: int):
-        self.strength = strength
+    def __init__(self, function: callable, **kwargs):
+        self.function = function
+        self.kwargs = kwargs
 
     def __call__(self, x: float, y: float) -> Tuple[float, float]:
         """This method returns the velocity field of the vector field at a given point.
@@ -24,9 +65,53 @@ class VectorField:
         Tuple[float, float]
             The velocity field at the given point
         """
-        u = -self.strength * y
-        v = self.strength * x
-        return u, v
+        return self.function(x, y, **self.kwargs)
+
+    def dx(self, x: float, y: float) -> Tuple[float, float]:
+        """This method computes the derivate of the x-component of the vector field.
+        Uses finite difference method.
+
+        Parameters
+        ----------
+        x : float
+            Position in the x-direction
+        y : float
+            Position in the y-direction
+
+        Returns
+        -------
+        Tuple[float, float]
+            The derivated velocity field at the given point
+        """
+        delta = 1e-6
+        f = self(x, y)
+        fd = self(x + delta, y)
+        dudx = (fd[0] - f[0]) / delta
+        dvdx = (fd[1] - f[1]) / delta
+        return dudx, dvdx
+
+    def dy(self, x: float, y: float) -> Tuple[float, float]:
+        """This method computes the derivate of the y-component of the vector field.
+        Uses finite difference method.
+
+        Parameters
+        ----------
+        x : float
+            Position in the x-direction
+        y : float
+            Position in the y-direction
+
+        Returns
+        -------
+        Tuple[float, float]
+            The derivated velocity field at the given point
+        """
+        delta = 1e-6
+        f = self(x, y)
+        fd = self(x, y + delta)
+        dudy = (fd[0] - f[0]) / delta
+        dvdy = (fd[1] - f[1]) / delta
+        return dudy, dvdy
 
     def plot(self, bounds: Tuple[int, int, int, int], step: float = 0.1):
         """This method plots the vector field.
@@ -65,7 +150,13 @@ def test_plot(vf: VectorField):
 
 
 def main():
-    vf = VectorField(1)
+    print("Testing the VectorField class with horizontal:")
+    vf = VectorField(horizontal_current, strength=1)
+    test_call(vf)
+    test_plot(vf)
+
+    print("Testing the VectorField class with circular:")
+    vf = VectorField(circular_current, strength=1)
     test_call(vf)
     test_plot(vf)
 
