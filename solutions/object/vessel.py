21118f12df500c259fd43b094cca331e8e96c9d6
Daniel Precioso <danipg1409@gmail.com>
2024-09-23 16:22:28 +0200

    Change speed with thrust, add more functions

--- solutions/object/vessel.py
+++ solutions/object/vessel.py
@@ -1,3 +1,5 @@
+from math import atan2, cos, sin
+
 import matplotlib.pyplot as plt
 from vectorfield import VectorField, circular_current
 
@@ -8,14 +10,24 @@ class Vessel:
         vectorfield: VectorField,
         x: float = 0,
         y: float = 0,
-        speed: float = 0,
+        thrust: float = 0,
         theta: float = 0,
     ):
         self.vectorfield = vectorfield
         self.x = x
         self.y = y
-        self.speed = speed
+        self.thrust = thrust
         self.theta = theta
+        # Hidden attributes to store the plot of the vessel
+        self._plot_point = None
+
+    @property
+    def thrust_x(self) -> float:
+        return self.thrust * cos(self.theta)
+
+    @property
+    def thrust_y(self) -> float:
+        return self.thrust * sin(self.theta)
 
     def drift(self, dt: float):
         """This method computes the drift of the vessel over a given time interval."""
@@ -24,25 +36,104 @@ class Vessel:
         self.x += u * dt
         self.y += v * dt
 
+    def move(self, dt: float):
+        """This method moves the vessel in the direction of its heading."""
+        u, v = self.vectorfield(self.x, self.y)
+        dx = u + self.thrust_x
+        dy = v + self.thrust_y
+        self.x += dx * dt
+        self.y += dy * dt
 
-def test_drift(vessel: Vessel):
-    vessel.vectorfield.plot([-5, 5, -5, 5])
-    dt = 0.05
-    pt = plt.scatter(vessel.x, vessel.y, color="red")
+    def head_to(self, x: float, y: float, dt: float):
+        """This method moves the vessel to a given position."""
+        dx = x - self.x
+        dy = y - self.y
+        self.theta = atan2(dy, dx)
+        self.move(dt)
+
+    def plot(self):
+        # Remove the previous plot if it exists
+        if self._plot_point:
+            self._plot_point.remove()
+            self._plot_arrow.remove()
+        # Plot the vessel at its current heading
+        self._plot_point = plt.scatter(self.x, self.y, color="red")
+        self._plot_arrow = plt.arrow(
+            self.x,
+            self.y,
+            self.thrust_x / 2,
+            self.thrust_y / 2,
+            color="red",
+            head_width=self.thrust / 10,
+        )
+
+
+def test_drift(vectorfield: VectorField, dt: float = 0.05):
+    vessel = Vessel(vectorfield, x=2, y=2, thrust=0, theta=0)
+    vectorfield.plot([-5, 5, -5, 5])
     # Start an animation to show the drift of the vessel
     # User controls when it stops
     while True:
         vessel.drift(dt)
-        pt.set_offsets([vessel.x, vessel.y])
-        plt.pause(0.1)
+        vessel.plot()
+        plt.pause(dt)
+        if not plt.get_fignums():
+            break
+
+
+def test_drift_multiple(vectorfield: VectorField, dt: float = 0.05):
+    ls_vessels: list[Vessel] = []
+
+    for x in range(4):
+        for y in range(4):
+            ls_vessels.append(Vessel(vectorfield, x=x, y=y, thrust=0, theta=0))
+
+    vectorfield.plot([-5, 5, -5, 5])
+    # Start an animation to show the movement of the vessel
+    # User controls when it stops
+    while True:
+        for vessel in ls_vessels:
+            vessel.drift(dt)
+            vessel.plot()
+        plt.pause(dt)
+        if not plt.get_fignums():
+            break
+
+
+def test_move(vectorfield: VectorField, dt: float = 0.05):
+    vessel = Vessel(vectorfield, x=2, y=2, thrust=2, theta=0)
+    vectorfield.plot([-5, 5, -5, 5])
+    # Start an animation to show the movement of the vessel
+    # User controls when it stops
+    while True:
+        vessel.move(dt)
+        vessel.plot()
+        plt.pause(dt)
+        if not plt.get_fignums():
+            break
+
+
+def test_head_to(vectorfield: VectorField, dt: float = 0.05):
+    vessel = Vessel(vectorfield, x=2, y=2, thrust=2, theta=0)
+    x, y = -2, -2
+    vectorfield.plot([-5, 5, -5, 5])
+    plt.scatter(x, y, color="blue")
+    # Start an animation to show the movement of the vessel
+    # User controls when it stops
+    while True:
+        vessel.head_to(x, y, dt)
+        vessel.plot()
+        plt.pause(dt)
         if not plt.get_fignums():
             break
 
 
 def main():
     vectorfield = VectorField(circular_current)
-    vessel = Vessel(vectorfield, x=2, y=2, speed=1, theta=0)
-    test_drift(vessel)
+    test_drift(vectorfield)
+    test_drift_multiple(vectorfield)
+    test_move(vectorfield)
+    test_head_to(vectorfield)
 
 
 if __name__ == "__main__":
