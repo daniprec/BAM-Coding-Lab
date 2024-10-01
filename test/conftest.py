0279f6a231c4984bcbc4a637f4991367626c0d44
daniprec <daniprec@users.noreply.github.com>
2024-10-01 11:40:51 +0000

    Format code with black

--- test/conftest.py
+++ test/conftest.py
@@ -2,7 +2,9 @@ import pytest
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
-    parser.addoption("--special", action="store_true", default=False, help="run special tests")
+    parser.addoption(
+        "--special", action="store_true", default=False, help="run special tests"
+    )
 
 
 def pytest_configure(config: pytest.Config) -> None:
